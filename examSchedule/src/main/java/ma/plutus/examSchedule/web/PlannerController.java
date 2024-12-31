72980dc71a9d214029174c6e2da16706be00b8e5
Abdellatif Hour <abdellatif.hour.web@gmail.com>
2024-12-31 17:05:00 +0100

    Update PlannerController.java

--- examSchedule/src/main/java/ma/plutus/examSchedule/web/PlannerController.java
+++ examSchedule/src/main/java/ma/plutus/examSchedule/web/PlannerController.java
@@ -1,15 +1,22 @@
 package ma.plutus.examSchedule.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import ai.timefold.solver.core.api.score.analysis.ScoreAnalysis;
+import ai.timefold.solver.core.api.score.buildin.hardsoftlong.HardSoftLongScore;
+import ai.timefold.solver.core.api.solver.ScoreAnalysisFetchPolicy;
 import ai.timefold.solver.core.api.solver.Solver;
 import ma.plutus.examSchedule.dtos.ExamSolution;
 
 
 @RestController
+@RequestMapping("/exam")
 public class PlannerController {
     
 
@@ -28,9 +35,16 @@ public class PlannerController {
 
 
 
-    @PostMapping("/exam/solve")
+    @PostMapping("/solve")
     public ExamSolution solveExamProblem(@RequestBody ExamSolution examProblem) {
         
         return examSolver.solve(examProblem) ;
     }
+
+
+    // @GetMapping("/analyze")
+    // public ScoreAnalysis<HardSoftLongScore> analyze(ExamSolution problem,
+    //                                                 @RequestParam("fetchPolicy") ScoreAnalysisFetchPolicy fetchPolicy) {
+    //     return fetchPolicy == null ? examSolver.analyze(problem) : examSolver.analyze(problem, fetchPolicy);
+    // }
 }
