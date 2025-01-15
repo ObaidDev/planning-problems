937ae6cc59ca6a7cd7b59a4f62b4945eda6fea3a
Abdellatif Hour <abdellatif.hour.web@gmail.com>
2025-01-15 11:11:29 +0100

    Start the Vehicle routing palning problem

---
+++ vehicle-routing/src/main/java/ma/plutus/vehicle_routing/VehicleRoutingApplication.java
@@ -0,0 +1,13 @@
+package ma.plutus.vehicle_routing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleRoutingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleRoutingApplication.class, args);
+	}
+
+}
