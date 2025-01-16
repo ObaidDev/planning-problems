3261bfd0d6cfba2a32e551357f7216f5a46a771b
Abdellatif Hour <abdellatif.hour.web@gmail.com>
2025-01-16 17:17:12 +0100

    Update VehicleRoutePlanController

--- vehicle-routing/src/main/java/ma/plutus/vehicle_routing/web/VehicleRoutePlanController.java
+++ vehicle-routing/src/main/java/ma/plutus/vehicle_routing/web/VehicleRoutePlanController.java
@@ -1,8 +1,5 @@
 package ma.plutus.vehicle_routing.web;
 
-import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,15 +9,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import ai.timefold.solver.core.api.score.buildin.hardsoftlong.HardSoftLongScore;
-import ai.timefold.solver.core.api.solver.SolutionManager;
-import ai.timefold.solver.core.api.solver.SolverManager;
-import ai.timefold.solver.core.api.solver.SolverStatus;
+
 import lombok.extern.slf4j.Slf4j;
-import ma.plutus.vehicle_routing.dto.Job;
 import ma.plutus.vehicle_routing.dto.VehicleRouteSolution;
 import ma.plutus.vehicle_routing.services.VehicleRoutingService;
-import ma.plutus.vehicle_routing.utils.TTLConcurrentMap;
+
 
 @Slf4j
 @RestController
