06540e4409068501e4fbaea4e6303d5f70ef3ece
Abdellatif Hour <abdellatif.hour.web@gmail.com>
2025-01-21 13:52:03 +0100

    Write Test For data validation

--- vehicle-routing/src/test/java/ma/plutus/vehicle_routing/integration/VehicleRoutePlanControllerIntegrationTest.java
+++ vehicle-routing/src/test/java/ma/plutus/vehicle_routing/integration/VehicleRoutePlanControllerIntegrationTest.java
@@ -4,17 +4,22 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledInNativeImage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.JsonPath;
 
 import lombok.extern.slf4j.Slf4j;
 import ma.plutus.vehicle_routing.dto.Location;
@@ -46,11 +51,17 @@ public class VehicleRoutePlanControllerIntegrationTest extends BaseIntegrationTe
                                                                         .contentType(MediaType.APPLICATION_JSON)
                                                                         .content(jsonPayload)
                                                 )
-                                            .andExpect(MockMvcResultMatchers.status().isOk())
-                                            .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.TEXT_PLAIN))
+                                            .andExpect(MockMvcResultMatchers.status().isCreated())
+                                            .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
                                             .andReturn().getResponse().getContentAsString();
+        
+        Map<String,Object> response = objectMapper.readValue(
+            responseFromTestPostAPlan
+            , 
+            new TypeReference<Map<String,Object>>() {}
+            );
 
-        sharedResponse.set(responseFromTestPostAPlan);
+        sharedResponse.set(response.get("planingID").toString());
 
         log.info("Response from testPostAPlan: {} ✅", responseFromTestPostAPlan);
     }
@@ -82,16 +93,41 @@ public class VehicleRoutePlanControllerIntegrationTest extends BaseIntegrationTe
         String jsonPayload = objectMapper.writeValueAsString(invalidInput);
 
         // Act & Assert
-        mockMvc.perform(
-                MockMvcRequestBuilders.post("/routing/solve")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(jsonPayload)
-                )
-                .andExpect(MockMvcResultMatchers.status().isBadRequest())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.status").value(false))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Validation failed"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.errors[0].field").value("name"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.errors[0].message").value("Name cannot be null"));
+        MvcResult result = mockMvc.perform(
+                            MockMvcRequestBuilders.post("/routing/solve")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(jsonPayload)
+                            )
+                            .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                            .andExpect(MockMvcResultMatchers.jsonPath("$.status").exists())
+                            .andExpect(MockMvcResultMatchers.jsonPath("$.message").exists())
+                            .andExpect(MockMvcResultMatchers.jsonPath("$.errors").exists()) 
+                            .andExpect(MockMvcResultMatchers.jsonPath("$.errors[0].message").exists())
+                            .andReturn();
+
+        /***
+         * 
+         * assert the type of the field in the response 
+         */
+
+        String responseJson = result.getResponse().getContentAsString();
+        
+        /***
+         * assert message
+         */
+
+        String message = JsonPath.read(responseJson, "$.message");
+
+        Assertions.assertThat(message).isInstanceOf(String.class) ;
+
+
+
+        /***
+         * assert message
+         */
+
+        List<?> errors = JsonPath.read(responseJson, "$.errors");
+        Assertions.assertThat(errors).isInstanceOf(List.class);
     }
 
 
@@ -103,8 +139,7 @@ public class VehicleRoutePlanControllerIntegrationTest extends BaseIntegrationTe
 
 
     private VehicleRouteSolution createValidVehicleRouteSolution() {
-        Location southWestCorner = new Location(39.7656099067391, -76.83782328143754);
-        Location northEastCorner = new Location(40.77636644354855, -74.9300739430771);
+
         Location vehicleHomeLocation = new Location(40.605994321126936, -75.68106859680056);
         Location visitLocation = new Location(40.397480680074565, -76.05412711128848);
 
@@ -128,8 +163,6 @@ public class VehicleRoutePlanControllerIntegrationTest extends BaseIntegrationTe
 
         VehicleRouteSolution vehicleRouteSolution = new VehicleRouteSolution();
         vehicleRouteSolution.setName("demo");
-        vehicleRouteSolution.setSouthWestCorner(southWestCorner);
-        vehicleRouteSolution.setNorthEastCorner(northEastCorner);
         vehicleRouteSolution.setStartDateTime(LocalDateTime.of(2025, 1, 16, 7, 30));
         vehicleRouteSolution.setEndDateTime(LocalDateTime.of(2025, 1, 17, 0, 0));
         vehicleRouteSolution.setVehicles(List.of(vehicle));
