32f92946cd72ed4679d12117fd64e6a78c043384
Abdellatif Hour <abdellatif.hour.web@gmail.com>
2025-01-21 13:07:52 +0100

    Handel the Exectptions Thorw from validtion and return Message about they

--- vehicle-routing/src/main/java/ma/plutus/vehicle_routing/exception/GlobalExceptionHandler.java
+++ vehicle-routing/src/main/java/ma/plutus/vehicle_routing/exception/GlobalExceptionHandler.java
@@ -2,14 +2,19 @@ package ma.plutus.vehicle_routing.exception;
 
 
 import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import io.swagger.v3.oas.annotations.Hidden;
+import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import ma.plutus.vehicle_routing.dto.ErrorResponse;
+import ma.plutus.vehicle_routing.dto.ErrorResponse.ErrorDetail;
 
 
 @Slf4j
@@ -34,6 +39,42 @@ public class GlobalExceptionHandler {
 
 
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex){
+
+
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setStatus(false);
+        errorResponse.setMessage("Validation failed");
+
+        // Extract validation errors from the exception
+        List<ErrorResponse.ErrorDetail> validationErrors = ex.getBindingResult().getFieldErrors().stream()
+            .map(fieldError -> {
+                ErrorResponse.ErrorDetail errorDetail = new ErrorResponse.ErrorDetail();
+                errorDetail.setField(fieldError.getField()); // Get the field name
+                errorDetail.setMessage(fieldError.getDefaultMessage()); // Get the error message
+                return errorDetail;
+            })
+            .collect(Collectors.toList());
+
+        errorResponse.setErrors(validationErrors);
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+
+    }
+
+
+
+
+
+
+    private String extractSimpleFieldName(String propertyPath) {
+        String[] parts = propertyPath.split("\\.");
+        return parts[parts.length - 1];
     }
     
 }
